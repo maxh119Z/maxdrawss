193d2333f9e27f42fc427308b4045611e32d82a5
lzhangcs <63130594+lzhangcs@users.noreply.github.com>
2024-12-25 15:26:02 -0800

    Add project files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/maxdrawss/', // Required for GitHub Pages
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // Optional for debugging production builds
+  },
 });
